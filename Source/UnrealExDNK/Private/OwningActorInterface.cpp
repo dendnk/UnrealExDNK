8d07265bb62776109d0c2c34066c737ae9f35bb3
Denis Kruchok <denis.kruchok.cv@gmail.com>
2024-12-31 00:52:48 +0300

    Added owning actor interface

---
+++ Source/UnrealExDNK/Private/OwningActorInterface.cpp
@@ -0,0 +1,7 @@
+// Copyright 2024 [HeliAce: Denis Kruchok, Denis Romanovsky]. All rights reserved.
+
+
+#include "OwningActorInterface.h"
+
+
+// Add default functionality here for any IOwningActorInterface functions that are not pure virtual.
---
+++ Source/UnrealExDNK/Public/OwningActorInterface.h
@@ -0,0 +1,24 @@
+// Copyright 2024 [HeliAce: Denis Kruchok, Denis Romanovsky]. All rights reserved.
+
+#pragma once
+
+#include "UObject/Interface.h"
+#include "OwningActorInterface.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UOwningActorInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+* Interface for retrieving the actor that owns or is associated with the implementing object.
+*/
+class UNREALEXDNK_API IOwningActorInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = OwningActor)
+	AActor* GetOwningActor() const;	
+};
