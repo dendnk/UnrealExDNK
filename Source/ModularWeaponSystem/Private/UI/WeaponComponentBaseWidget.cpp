4a8ebf520c3acc248be13f16988de3e0a715b9c8
Denis Kruchok <denis.kruchok.cv@gmail.com>
2025-07-24 02:34:48 +0300

    Added FireType, Max Ammo, Current Ammo, Ammo Per Shot, Infinite Ammo to weapon base widget

--- Source/ModularWeaponSystem/Private/UI/WeaponComponentBaseWidget.cpp
+++ Source/ModularWeaponSystem/Private/UI/WeaponComponentBaseWidget.cpp
@@ -2,7 +2,8 @@
 
 
 #include "UI/WeaponComponentBaseWidget.h"
-#include "Components/ComboBoxString.h"
+#include "Components/CheckBox.h"
+#include "Components/EditableTextBox.h"
 #include "UI/WeaponViewModel.h"
 #include "UnrealExDNKUtils.h"
 #include "UObject/Class.h"
@@ -28,47 +29,106 @@ void UWeaponComponentBaseWidget::SetViewModel(UWeaponViewModel* InViewModel)
 
 void UWeaponComponentBaseWidget::UpdateUIFromViewModel()
 {
-	if (IsValid(FiringModeComboBox))
+	if (IsValid(ViewModel) == false)
 	{
-		PopulateFiringModeOptions();
+		UE_DNK_LOG(LogTemp, Error, "ViewModel is invalid!");
+		return;
+	}
 
-		// Bind selection changed event
-		FiringModeComboBox->OnSelectionChanged.AddDynamic(this, &ThisClass::OnFiringModeChanged);
+	bool bAllValid = true;
 
-		// Optional: set initial selection
-		if (IsValid(ViewModel))
+	const TArray<TPair<FString, UObject*>> WidgetChecks = {
+		{ TEXT("FireTypeComboBox"), FireTypeComboBox },
+		{ TEXT("FiringModeComboBox"), FiringModeComboBox },
+		{ TEXT("MaxAmmoTextBox"), MaxAmmoTextBox },
+		{ TEXT("AmmoPerShotTextBox"), AmmoPerShotTextBox },
+		{ TEXT("CurrentAmmoTextBox"), CurrentAmmoTextBox },
+		{ TEXT("InfiniteAmmoCheckBox"), InfiniteAmmoCheckBox }
+	};
+
+	for (const auto& [Name, Widget] : WidgetChecks)
+	{
+		if (IsValid(Widget) == false)
 		{
-			FString Current = ViewModel->GetFiringModeAsString();
-			FiringModeComboBox->SetSelectedOption(Current);
+			bAllValid = false;
+			UE_DNK_LOG(LogTemp, Error, "%s is Invalid!", *Name);
 		}
 	}
+
+	if (!bAllValid)
+	{
+		UE_DNK_LOG(LogTemp, Error, "Some of the widgets were not found. Initialization aborted.");
+		return;
+	}
+
+	PopulateEnumOptions<EFiringMode>(FireTypeComboBox);
+	PopulateEnumOptions<EFiringMode>(FiringModeComboBox);
+
+	FireTypeComboBox->SetSelectedOption(ViewModel->GetFireTypeAsString());
+	FiringModeComboBox->SetSelectedOption(ViewModel->GetFiringModeAsString());
+	MaxAmmoTextBox->SetText(FText::FromString(FString::FromInt(ViewModel->GetMaxAmmo())));
+	AmmoPerShotTextBox->SetText(FText::FromString(FString::FromInt(ViewModel->GetAmmoPerShot())));
+	CurrentAmmoTextBox->SetText(FText::FromString(FString::FromInt(ViewModel->GetCurrentAmmo())));
+	InfiniteAmmoCheckBox->SetIsChecked(ViewModel->IsInfiniteAmmo());
+
+	// Bind selection changed event
+	FireTypeComboBox->OnSelectionChanged.AddDynamic(this, &ThisClass::OnFireTypeChanged);
+	FiringModeComboBox->OnSelectionChanged.AddDynamic(this, &ThisClass::OnFiringModeChanged);
+	MaxAmmoTextBox->OnTextCommitted.AddDynamic(this, &ThisClass::OnMaxAmmoCommitted);
+	AmmoPerShotTextBox->OnTextCommitted.AddDynamic(this, &ThisClass::OnAmmoPerShotCommitted);
+	CurrentAmmoTextBox->OnTextCommitted.AddDynamic(this, &ThisClass::OnCurrentAmmoCommitted);
+	InfiniteAmmoCheckBox->OnCheckStateChanged.AddDynamic(this, &ThisClass::OnInfiniteAmmoChanged);
 }
 
-void UWeaponComponentBaseWidget::PopulateFiringModeOptions()
+void UWeaponComponentBaseWidget::OnFireTypeChanged(FString SelectedItem, ESelectInfo::Type SelectionType)
 {
-	if (!FiringModeComboBox) return;
-
-	FiringModeComboBox->ClearOptions();
-	UEnum* Enum = StaticEnum<EFiringMode>();
-	if (!Enum) return;
-
-	for (int32 i = 0; i < Enum->NumEnums() - 1; ++i)
-	{
-		FString Option = Enum->GetNameStringByIndex(i);
-		FiringModeComboBox->AddOption(Option);
-	}
+	SetEnumFieldSafe<EFireType, UWeaponViewModel>(ViewModel, SelectedItem,
+		[](UWeaponViewModel* WeaponViewModel, EFireType FireType)
+		{
+			WeaponViewModel->SetFireType(FireType);
+		});
 }
 
 void UWeaponComponentBaseWidget::OnFiringModeChanged(FString SelectedItem, ESelectInfo::Type)
 {
-	if (!ViewModel) return;
+	SetEnumFieldSafe<EFiringMode, UWeaponViewModel>(ViewModel, SelectedItem,
+		[](UWeaponViewModel* WeaponViewModel, EFiringMode FiringMode)
+		{
+			WeaponViewModel->SetFiringMode(FiringMode);
+		});
+}
 
-	UEnum* Enum = StaticEnum<EFiringMode>();
-	if (!Enum) return;
-
-	int64 Value = Enum->GetValueByNameString(SelectedItem);
-	if (Enum->IsValidEnumValue(Value))
+void UWeaponComponentBaseWidget::OnMaxAmmoCommitted(const FText& Text, ETextCommit::Type CommitMethod)
+{
+	if (ViewModel && Text.IsNumeric())
 	{
-		ViewModel->SetFiringMode(static_cast<EFiringMode>(Value));
+		int32 Value = FCString::Atoi(*Text.ToString());
+		ViewModel->SetMaxAmmo(Value);
+	}
+}
+
+void UWeaponComponentBaseWidget::OnAmmoPerShotCommitted(const FText& Text, ETextCommit::Type CommitMethod)
+{
+	if (ViewModel && Text.IsNumeric())
+	{
+		int32 Value = FCString::Atoi(*Text.ToString());
+		ViewModel->SetAmmoPerShot(Value);
+	}
+}
+
+void UWeaponComponentBaseWidget::OnCurrentAmmoCommitted(const FText& Text, ETextCommit::Type CommitMethod)
+{
+	if (ViewModel && Text.IsNumeric())
+	{
+		int32 Value = FCString::Atoi(*Text.ToString());
+		ViewModel->SetCurrentAmmo(Value);
+	}
+}
+
+void UWeaponComponentBaseWidget::OnInfiniteAmmoChanged(bool bIsChecked)
+{
+	if (ViewModel)
+	{
+		ViewModel->SetIsInfiniteAmmo(bIsChecked);
 	}
 }
--- Source/ModularWeaponSystem/Public/UI/WeaponComponentBaseWidget.h
+++ Source/ModularWeaponSystem/Public/UI/WeaponComponentBaseWidget.h
@@ -3,9 +3,11 @@
 #pragma once
 
 #include "Blueprint/UserWidget.h"
+#include "Components/ComboBoxString.h"
 #include "WeaponComponentBaseWidget.generated.h"
 
-class UComboBoxString;
+class UCheckBox;
+class UEditableTextBox;
 class UWeaponViewModel;
 
 /**
@@ -28,17 +30,96 @@ protected:
 	UFUNCTION()
 	void UpdateUIFromViewModel();
 
-	// Callback for user changing the selected item
+	UFUNCTION()
+	void OnFireTypeChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
+
 	UFUNCTION()
 	void OnFiringModeChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
 
-	// Helper to populate options
-	void PopulateFiringModeOptions();
+	UFUNCTION()
+	void OnMaxAmmoCommitted(const FText& Text, ETextCommit::Type CommitMethod);
 
-	// ComboBox for selecting FiringMode
+	UFUNCTION()
+	void OnAmmoPerShotCommitted(const FText& Text, ETextCommit::Type CommitMethod);
+
+	UFUNCTION()
+	void OnCurrentAmmoCommitted(const FText& Text, ETextCommit::Type CommitMethod);
+
+	UFUNCTION()
+	void OnInfiniteAmmoChanged(bool bIsChecked);
+
+
 	UPROPERTY(meta = (BindWidget))
-	UComboBoxString* FiringModeComboBox;
+	TObjectPtr<UComboBoxString> FireTypeComboBox;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UComboBoxString> FiringModeComboBox;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> MaxAmmoTextBox;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> AmmoPerShotTextBox;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> CurrentAmmoTextBox;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCheckBox> InfiniteAmmoCheckBox;
+
 
 	UPROPERTY(Transient, meta = (BindWidgetViewModel))
 	TObjectPtr<UWeaponViewModel> ViewModel;
 };
+
+template<typename TEnum, typename TViewModel>
+void SetEnumFieldSafe(TObjectPtr<TViewModel> ViewModel, const FString& SelectedItem, TFunction<void(TViewModel*, TEnum)> Setter)
+{
+	static_assert(TIsDerivedFrom<TViewModel, UObject>::Value, "TViewModel must be a UObject");
+	static_assert(TIsEnum<TEnum>::Value, "TEnum must be an enumeration type.");
+
+	if (IsValid(ViewModel) == false)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ViewModel is invalid!"));
+		return;
+	}
+
+	static_assert(TIsEnum<TEnum>::Value, "TEnum must be an enumeration type.");
+
+	UEnum* Enum = StaticEnum<TEnum>();
+	if (!Enum)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Enum reflection missing for %s"), *FString(typeid(TEnum).name()));
+		return;
+	}
+
+	int64 Value = Enum->GetValueByNameString(SelectedItem);
+	if (Enum->IsValidEnumValue(Value))
+	{
+		Setter(ViewModel, static_cast<TEnum>(Value));
+	}
+}
+
+template<typename TEnum>
+void PopulateEnumOptions(UComboBoxString* ComboBox)
+{
+	if (IsValid(ComboBox) == false)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ComboBox is invalid!"));
+		return;
+	}
+
+	ComboBox->ClearOptions();
+
+	if (UEnum* Enum = StaticEnum<TEnum>())
+	{
+		for (int32 i = 0; i < Enum->NumEnums(); ++i)
+		{
+			if (!Enum->HasMetaData(TEXT("Hidden"), i))
+			{
+				FString Name = Enum->GetNameStringByIndex(i);
+				ComboBox->AddOption(Name);
+			}
+		}
+	}
+}
